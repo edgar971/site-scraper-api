ae514dc03a2d9652f29d573b2c24ad9f82336cfc
Edgar Pino <edgarbeltran@me.com>
2018-02-01 23:02:42 -0700

    remove unused fields

--- common/interfaces.ts
+++ common/interfaces.ts
@@ -1,12 +1,11 @@
 export interface Site {
   id?: number,
-  url?: string,
+  url: string,
   directory?: string,
   base_path?: string,
-  title: string,
+  title?: string,
   screenshot?: string,
   entire_site: boolean,
-  processed: boolean,
-  created_at?: Date,
-  updated_at?: Date
+  processed?: boolean,
+  created_at?: Date
 }

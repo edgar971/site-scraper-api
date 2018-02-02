462e99fcf42642f245df23bff3cd41c9cd6081d1
Edgar Pino <edgarbeltran@me.com>
2018-02-01 23:03:53 -0700

    check for env in process

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,8 @@
 import { Config } from './interfaces'
 const database = require('./database.json')
 const config = require('./config.json') as Config
+const env = process.env.ENV || config.env
 
-config.database = database[config.env]
+config.database = database[env]
 
 export default config

bd68f9d8414a8131e304f1aa7351ebb5aabe9f02
Edgar Pino <edgarbeltran@me.com>
2018-02-18 20:28:14 -0700

    close browser after running

--- scraper/__integration_specs__/index.specs.ts
+++ scraper/__integration_specs__/index.specs.ts
@@ -1,18 +1,24 @@
 import * as chai from 'chai'
 import { getHTMLContent } from '../page';
+import { closeBrowser } from '../puppeteer/index'
 
 chai.should()
 
-context('#Scraper integration tests', () => {
+context.only('#Scraper integration tests', () => {
   describe('when scraping a single successfully', () => {
     const targetWebsite = 'https://www.edgarpino.com/'
     let content
 
-    before(async ()=> {
+    before(async () => {
       content = await getHTMLContent(targetWebsite)
     });
 
     it('should return the HTML content', () => content.should.not.be.null)
-    
+
   })
+
+  after(async () => {
+    await closeBrowser()
+  })
+
 })
